351fb38c5510211499ccf191f73ad8e3fd7f772c
GioLucci <141793843+Vannasaur@users.noreply.github.com>
2023-10-31 14:45:30 -0700

    renamed folders, safe revert point

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // Bringing in the required import from 'react-router-dom'
 import { Outlet } from "react-router-dom";
-import Header from "./components/headerComp.jsx";
-import Footer from "./components/footerComp.jsx";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
   // The Outlet component will conditionally swap between the different pages according to the URL
