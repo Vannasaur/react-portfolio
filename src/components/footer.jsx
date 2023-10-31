cc0b297b86ffe1bacab717c179595a6201af2e9f
GioLucci <141793843+Vannasaur@users.noreply.github.com>
2023-10-31 14:36:53 -0700

    updated header and footer, safe revert point

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,21 +3,21 @@ function Footer() {
 		<footer className="footer">
 			<div className="footer-links">
 				<a href="https://github.com/Vannasaur" target="_blank" className="socials">
-					<img src="../../public/images/github.svg" className="github"></img>
+					<img src="/images/github.svg" className="github"></img>
 				</a>
 				<a href="https://www.linkedin.com/in/giovanna-luciano/" target="_blank" className="socials">
 					<img
-						src="../../public/images/linkedin.svg"
+						src="/images/linkedin.svg"
 						className="linkedin"
 					></img>
 				</a>
 				<a href="https://www.instagram.com/luccii.creates/" target="_blank" className="socials">
-					<img src="../../public/images/ig.svg" className="instagram"></img>
+					<img src="/images/ig.svg" className="instagram"></img>
 				</a>
 			</div>
 			<div className="footer-logo">
 				<img
-					src="../../public/images/LucciiDesignHotPink.svg"
+					src="/images/LucciiDesignHotPink.svg"
 					className="logo"
 				></img>
 			</div>
