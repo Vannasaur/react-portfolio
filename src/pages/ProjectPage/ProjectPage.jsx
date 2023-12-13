2dd1f827d5a5dde3f51594c5578384622eb06425
GioLucci <141793843+Vannasaur@users.noreply.github.com>
2023-12-13 15:09:16 -0800

    updated image on contact page, updated projects, safe revert point

--- src/pages/ProjectPage/ProjectPage.jsx
+++ src/pages/ProjectPage/ProjectPage.jsx
@@ -1,106 +1,75 @@
+import React from 'react';
+
+const Project = ({ imgSrc, title, appLink, githubLink }) => {
+	return (
+		<figure className="project">
+			<img src={imgSrc} className="project-img" />
+			<figcaption className="textbox">
+				<h3>{title}</h3>
+				<a href={appLink} target="_blank">
+					<button className="project-btn">Application</button>
+				</a>
+				<a href={githubLink} target="_blank">
+					<button className="project-btn">GitHub</button>
+				</a>
+			</figcaption>
+		</figure>
+	);
+};
+
 const ProjectPage = () => {
+	const projects = [
+		{
+			imgSrc: '/images/projects/JATE.jpeg',
+			title: 'Text Editor',
+			appLink: 'https://aqueous-earth-61873-b9d107970637.herokuapp.com/',
+			githubLink: 'https://github.com/Vannasaur/text-editor',
+		},
+		{
+			imgSrc: '/images/projects/weather-dashboard.png',
+			title: 'Weather Dashboard',
+			appLink: 'https://vannasaur.github.io/weather-dashboard/',
+			githubLink: 'https://github.com/Vannasaur/weather-dashboard',
+		},
+		{
+			imgSrc: '/images/projects/blog.png',
+			title: 'Blog',
+			appLink: 'https://immense-caverns-15186-3cc45d438581.herokuapp.com/',
+			githubLink: 'https://github.com/Vannasaur/tech-blog',
+		},
+		{
+			imgSrc: '/images/projects/help-desk.jpeg',
+			title: 'Help Desk',
+			appLink: 'https://murmuring-chamber-70429-75790581b652.herokuapp.com/',
+			githubLink: 'https://github.com/Vannasaur/girls-help-desk',
+		},
+		{
+			imgSrc: '/images/projects/will-do.png',
+			title: 'Will Do: Crush Your Goals!',
+			appLink: 'https://will-do-7ujl.onrender.com/',
+			githubLink: 'https://github.com/kyleochata/Will-DO-Crush-your-goals',
+		},
+		{
+			imgSrc: '/images/projects/codequiz.png',
+			title: 'Code Quiz',
+			appLink: 'https://vannasaur.github.io/code-quiz/',
+			githubLink: 'https://github.com/Vannasaur/code-quiz',
+		},
+	];
+
 	return (
 		<div className="projects">
-			<figure className="project">
-				<img src="/images/projects/JATE.jpeg" className="project-img" />
-				<figcaption className="textbox">
-					<h3>Text Editor</h3>
-					<a
-						href="https://aqueous-earth-61873-b9d107970637.herokuapp.com/"
-						target="_blank"
-					>
-						<button className="project-btn">Application</button>
-					</a>
-					<a href="https://github.com/Vannasaur/text-editor" target="_blank">
-						<button className="project-btn">GitHub</button>
-					</a>
-				</figcaption>
-			</figure>
-			<figure className="project">
-				<img
-					src="/images/projects/weather-dashboard.png"
-					className="project-img"
+			{projects.map((project, index) => (
+				<Project
+					key={index}
+					imgSrc={project.imgSrc}
+					title={project.title}
+					appLink={project.appLink}
+					githubLink={project.githubLink}
 				/>
-				<figcaption className="textbox">
-					<h3>Weather Dashboard</h3>
-					<a
-						href="https://vannasaur.github.io/weather-dashboard/"
-						target="_blank"
-					>
-						<button className="project-btn">Application</button>
-					</a>
-					<a
-						href="https://github.com/Vannasaur/weather-dashboard"
-						target="_blank"
-					>
-						<button className="project-btn">GitHub</button>
-					</a>
-				</figcaption>
-			</figure>
-			<figure className="project">
-				<img src="/images/projects/blog.png" className="project-img" />
-				<figcaption className="textbox">
-					<h3>Blog</h3>
-					<a
-						href="https://immense-caverns-15186-3cc45d438581.herokuapp.com/"
-						target="_blank"
-					>
-						<button className="project-btn">Application</button>
-					</a>
-					<a href="https://github.com/Vannasaur/tech-blog" target="_blank">
-						<button className="project-btn">GitHub</button>
-					</a>
-				</figcaption>
-			</figure>
-			<figure className="project">
-				<img src="/images/projects/help-desk.jpeg" className="project-img" />
-				<figcaption className="textbox">
-					<h3>Help Desk</h3>
-					<a
-						href="https://murmuring-chamber-70429-75790581b652.herokuapp.com/"
-						target="_blank"
-					>
-						<button className="project-btn">Application</button>
-					</a>
-					<a
-						href="https://github.com/Vannasaur/girls-help-desk"
-						target="_blank"
-					>
-						<button className="project-btn">GitHub</button>
-					</a>
-				</figcaption>
-			</figure>
-			<figure className="project">
-				<img
-					src="/images/projects/will-do.png"
-					className="project-img"
-				/>
-				<figcaption className="textbox">
-					<h3>Will Do: Crush Your Goals!</h3>
-					<a href="https://will-do-7ujl.onrender.com/" target="_blank">
-						<button className="project-btn">Application</button>
-					</a>
-					<a
-						href="https://github.com/kyleochata/Will-DO-Crush-your-goals"
-						target="_blank"
-					>
-						<button className="project-btn">GitHub</button>
-					</a>
-				</figcaption>
-			</figure>
-			<figure className="project">
-				<img src="/images/projects/codequiz.png" className="project-img" />
-				<figcaption className="textbox">
-					<h3>Code Quiz</h3>
-					<a href="https://vannasaur.github.io/code-quiz/" target="_blank">
-						<button className="project-btn">Application</button>
-					</a>
-					<a href="https://github.com/Vannasaur/code-quiz" target="_blank">
-						<button className="project-btn">GitHub</button>
-					</a>
-				</figcaption>
-			</figure>
+			))}
 		</div>
 	);
 };
+
 export default ProjectPage;
